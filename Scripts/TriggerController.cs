adf591cc207bbd021c4f0855c793a25191c23e0a
FineasNigelus <42266737+FineasNigelus@users.noreply.github.com>
2021-04-11 20:11:18 +0200

    Add files via upload

---
+++ Scripts/TriggerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            myAnimationController.SetBool("inObject", true);
+            //other.GetComponent<Animation>().Play();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            myAnimationController.SetBool("inObject", false);
+        }
+    }
+}
+
+
